7674b9334e3417a2d0f79aa311c64bb94dc3df8a
brandon kwamou <b.kwamoutchamou@gmail.com>
2022-12-10 16:31:51 +0100

    validating Request Data with pipes

--- dist/messages/messages.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { CreateMessageDto } from "./dto/create-message.dto";
-export declare class MessagesController {
-    getMessagesRoot(): string;
-    getMessagesId(id: string): string;
-    postMessages(createMessageDto: CreateMessageDto): string;
-}
