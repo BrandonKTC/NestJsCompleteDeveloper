4eb875e286ba10ab440982d1bfbd302df958a285
brandon kwamou <b.kwamoutchamou@gmail.com>
2022-12-11 12:23:51 +0100

    Persisting data with TypeORM

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,4 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { Report } from './report.entity';
+import { ReportsService } from './reports.service';
+import { ReportsController } from './reports.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Report])],
+  controllers: [ReportsController],
+  providers: [ReportsService],
+})
 export class ReportsModule {}
