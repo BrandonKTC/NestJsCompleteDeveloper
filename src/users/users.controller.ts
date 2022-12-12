416d621988252d0e05df197c45f17756f37324d4
brandon kwamou <b.kwamoutchamou@gmail.com>
2022-12-12 11:30:12 +0100

    Custom Data Serialization

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,10 +9,12 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
-
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
@@ -22,12 +24,15 @@ export class UsersController {
     return this.usersService.find(email);
   }
 
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get(':id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
+    console.log(user);
     if (!user) {
       throw new NotFoundException('User not found');
     }
+    return user;
   }
 
   @Post('signup')
